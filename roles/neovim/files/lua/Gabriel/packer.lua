0965a5e372b69353e0ce002ae2415f06c75b43b9
Gabriel Santos <gabrielopesantos97@gmail.com>
2022-10-16 16:06:24 +0100

    testing hop.nvim

--- roles/neovim/files/lua/Gabriel/packer.lua
+++ roles/neovim/files/lua/Gabriel/packer.lua
@@ -121,7 +121,7 @@ local function packer_startup()
   }
 
   -- Utilities
-  use 'unblevable/quick-scope' -- promote use of f<key>
+  --use 'unblevable/quick-scope' -- Promote use of f<key>
 
   use {
     'lukas-reineke/indent-blankline.nvim',
@@ -215,6 +215,32 @@ local function packer_startup()
     requires = {
       'nvim-lua/plenary.nvim'
     }
+  }
+
+  -- hop.nvim
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v2',
+    config = function()
+      local hop = require('hop')
+      local directions = require('hop.hint').HintDirection
+      vim.keymap.set('', 'f', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      end, {remap=true})
+      vim.keymap.set('', 'F', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      end, {remap=true})
+      vim.keymap.set('', 't', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end, {remap=true})
+      vim.keymap.set('', 'T', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      end, {remap=true})
+      vim.keymap.set('n', '<leader>hw', function()
+        hop.hint_words()
+      end, {remap=true})
+    require'hop'.setup()
+    end
   }
 
 end
