f44d44895be49b0516bbad8b060495339646070f
Gabriel Santos <gabrielopesantos97@gmail.com>
2022-10-30 14:24:18 +0000

    updates

--- roles/neovim/files/lua/Gabriel/packer.lua
+++ roles/neovim/files/lua/Gabriel/packer.lua
@@ -218,30 +218,30 @@ local function packer_startup()
   }
 
   -- hop.nvim
-  use {
-    'phaazon/hop.nvim',
-    branch = 'v2',
-    config = function()
-      local hop = require('hop')
-      local directions = require('hop.hint').HintDirection
-      vim.keymap.set('', 'f', function()
-        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-      end, {remap=true})
-      vim.keymap.set('', 'F', function()
-        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-      end, {remap=true})
-      vim.keymap.set('', 't', function()
-        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-      end, {remap=true})
-      vim.keymap.set('', 'T', function()
-        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-      end, {remap=true})
-      vim.keymap.set('n', '<leader>hw', function()
-        hop.hint_words()
-      end, {remap=true})
-    require'hop'.setup()
-    end
-  }
+  --use {
+    --'phaazon/hop.nvim',
+    --branch = 'v2',
+    --config = function()
+      --local hop = require('hop')
+      --local directions = require('hop.hint').HintDirection
+      --vim.keymap.set('', 'f', function()
+        --hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      --end, {remap=true})
+      --vim.keymap.set('', 'F', function()
+        --hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      --end, {remap=true})
+      --vim.keymap.set('', 't', function()
+        --hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      --end, {remap=true})
+      --vim.keymap.set('', 'T', function()
+        --hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      --end, {remap=true})
+      --vim.keymap.set('n', '<leader>hw', function()
+        --hop.hint_words()
+      --end, {remap=true})
+    --require'hop'.setup()
+    --end
+  --}
 
 end
 
